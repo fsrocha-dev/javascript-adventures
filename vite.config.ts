f9c8d26443d0fdb9752df508c45b10b2a681d7fe
fsrocha-dev <contato.frankrocha@gmail.com>
2022-08-10 09:08:20 -0300

    config markdown vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { Mode, plugin } from 'vite-plugin-markdown';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), plugin({ mode: [Mode.HTML] })]
 })
